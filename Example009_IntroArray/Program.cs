87d302b1c7d3e22de83bdcb29e7eaf59a18a0629
Tatyana Kapina <deniscapin@yandex.ru>
2023-07-04 17:41:07 +0300

    file example009, 010, 011

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,24 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}//             0   1   2   3   4   5   6   7   8
+int[] array = {11, 21, 31, 41, 15, 261, 17, 18, 19};  // требуется определить массив
+
+int result = Max(
+Max(array[0], array[1], array[2]),
+Max(array[3], array[4], array[5]),
+Max(array[6], array[7], array[8])
+);
+Console.WriteLine(result);
+//array[0] = 12;  // чтобы обратиться к конкретному элементу пишем имя нашего массива и в [используем его имя]
+//Console.WriteLine(array[4]);
+
+//int max = Max(
+//    Max(array, b1,c1),
+//    Max(a2, b2, c2),
+//    Max(a3, b3, c3));
+
+//Console.WriteLine(max);
---
+++ Example010_MathodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {1, 12, 31, 4, 15, 18, 16, 17, 18};
+
+int n = array.Length; // возвращает длину или количество элементов массива
+int find = 18;
+
+int index = 0;   // установить счетчик index
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+
